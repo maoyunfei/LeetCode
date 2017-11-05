33139e0e3eb71c6aba82f47ac3788010d69d8d2d
maoyunfei <735803352@qq.com>
2017-11-05 15:34:58 +0800

    add problem 617

---
+++ problems/src/MergeTwoBinaryTrees/README.md
@@ -0,0 +1,3 @@
+[Merge Two Binary Trees](https://leetcode.com/problems/merge-two-binary-trees/description/)
+
+这道题用递归很容易解决。
---
+++ problems/src/MergeTwoBinaryTrees/TreeNode.java
@@ -0,0 +1,17 @@
+package MergeTwoBinaryTrees;
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author maoyunfei
+ * @Date 2017/11/5
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
